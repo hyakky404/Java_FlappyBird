4b0a19b20f9599bc995fdbb887976d90780e8ba6
Nguyen Quy <85392867+ngquy02@users.noreply.github.com>
2022-01-20 15:06:42 +0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,8 @@
 
-public class hello {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class hello
+{
+	public static void main(String[] args)
+	{
 		System.out.print("Hello, World!");
 	}
-
 }
