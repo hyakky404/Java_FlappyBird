8339821ccda243a328eda49871acaaa41768bff6
Nguyen Quy <85392867+ngquy02@users.noreply.github.com>
2022-03-21 11:26:25 +0700

    Cập nhật 0.3.1
    Sửa một số lỗi

---
+++ FlappyBird/src/flappyBird/Renderer.java
@@ -0,0 +1,16 @@
+package flappyBird;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel
+{
+    private static final long serialVersionUID = 1L;
+    
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        FlappyBird.flappyBird.scene(g);
+    }
+}
